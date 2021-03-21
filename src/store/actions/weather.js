f2654fdb90ba1a87111f20212b21c96b3d05e4a9
Alexsandr <alexnem@inbox.ru>
2021-03-21 11:21:44 +0300

    refactor redux store; add current weather

--- src/store/actions/weather.js
+++ src/store/actions/weather.js
@@ -1,14 +1,19 @@
 import { fetchWeatherData } from '../../api/weather';
 import * as actionTypes from '../actionTypes';
 import { setIsInitialState, setIsLoading } from './app';
-import { kelvinToCelcius } from '../../utils/unitConversion';
+import { hPaToMMHg, kelvinToCelcius, localeTime } from '../../utils/unitConversion';
 
 export const fetchWeatherStart = () => ({
   type: actionTypes.FETCH_WEATHER_START,
 });
 
+export const fetchCurrentWeatherSuccess = (weather) => ({
+  type: actionTypes.FETCH_CURRENT_WEATHER_SUCCESS,
+  payload: { weather }
+});
+
 export const fetchWeatherSuccess = (weather, id) => ({
-  type: actionTypes.FETCH_WEATHER_SUCCESS,
+  type: actionTypes.FETCH_LIST_WEATHER_SUCCESS,
   payload: { weather }, id
 });
 
@@ -27,6 +32,12 @@ const transformWeatherData = (res) => {
     feels_like: kelvinToCelcius(weather.main.feels_like),
     temp_max: kelvinToCelcius(weather.main.temp_max),
     temp_min: kelvinToCelcius(weather.main.temp_min),
+    pressure: hPaToMMHg(weather.main.pressure),
+  };
+  weather.sys = {
+    ...weather.sys,
+    sunrise: localeTime(weather.sys.sunrise),
+    sunset: localeTime(weather.sys.sunset),
   };
   weather.wind.speed = Math.round(weather.wind.speed * 3.6);
 
@@ -34,6 +45,25 @@ const transformWeatherData = (res) => {
     weather,
   };
 };
+
+export const fetchCurrentWeatherFromApi = (city) => {
+  return (dispatch) => {
+    dispatch(setIsLoading(true));
+    dispatch(fetchWeatherStart());
+
+    Promise.all([fetchWeatherData(city)])
+      .then((res) => {
+        return Promise.all([res[0].json()]);
+      })
+      .then((res) => {
+        console.log(res, 'res');
+        const { weather } = transformWeatherData(res);
+        dispatch(fetchCurrentWeatherSuccess(weather));
+        dispatch(setIsInitialState(false));
+        dispatch(setIsLoading(false));
+      })
+  }
+}
 
 export const fetchWeatherFromApi = (city, id, lat, lng) => {
   return (dispatch) => {
--- src/store/reducers/weatherReducer.js
+++ src/store/reducers/weatherReducer.js
@@ -1,32 +1,8 @@
 import * as actionTypes from '../actionTypes';
 
 const initialState = {
-  weatherData: {
-    main: {
-      feels_like: 0,
-      humidity: 0,
-      pressure: 0,
-      temp: 0,
-      temp_max: 0,
-      temp_min: 0,
-    },
-    name: '',
-    sys: {
-      country: '',
-      sunrise: 0,
-      sunset: 0,
-    },
-    weather: {
-      id: 200,
-      main: '',
-      description: '',
-      icon: '',
-    },
-    wind: {
-      deg: 0,
-      speed: 0,
-    },
-  },
+  weatherData: {},
+  currentWeather: null,
   extendedWeatherData: [],
   isError: false,
 };
@@ -35,10 +11,15 @@ export const weatherReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_WEATHER_START:
       return state;
-    case actionTypes.FETCH_WEATHER_SUCCESS:
+    case actionTypes.FETCH_LIST_WEATHER_SUCCESS:
       return {
         ...state,
         weatherData: {...state.weatherData, [action.id]: action.payload.weather },
+      };
+    case actionTypes.FETCH_CURRENT_WEATHER_SUCCESS:
+      return {
+        ...state,
+        currentWeather: action.payload.weather,
       };
     case actionTypes.FETCH_WEATHER_ERROR:
       return {
